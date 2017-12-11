7047ab326e74e3fbd00e933ba0d149f2bed4fccf
Vladimir Solomykin <scordavis@gmail.com>
2017-12-11 11:55:09 +0200

    create and include dynamic header slider
    создан файл слайдера и подключен в хэдер главной

--- index.php
+++ index.php
@@ -14,67 +14,9 @@
 
 get_header(); ?>
 
-	<!-- start slider -->
-	<div class="slider-container">
-		<div class="slider-control left inactive"></div>
-		<div class="slider-control right"></div>
-		<ul class="slider-pagi"></ul>
-		<div class="slider">
-			<!-- start slide -->
-			<div class="slide slide-0 active">
-				<div class="slide__bg" style="background-image: url(<?php echo get_template_directory_uri() ?>/img/slide_1.jpg);"></div>
-				<div class="slide__content">
-					<div class="container slide__content-wrap">
-						<section class="slide__text">
-							<h2 class="slide__text-heading">Новые поступления &mdash; <br>новые впечатления!</h2>
-							<p class="slide__text-desc"></p> <a href="catalog.html#brewers" class="slide__text-link">подробнее</a> 
-						</section>
-					</div>
-				</div>
-			</div>
-			<!-- end slide -->
-			<!-- start slide -->
-			<div class="slide slide-1 ">
-				<div class="slide__bg" style="background-image: url(<?php echo get_template_directory_uri() ?>/img/slide_2.jpg);"></div>
-				<div class="slide__content">
-					<div class="container slide__content-wrap">
-						<section class="slide__text">
-							<h2 class="slide__text-heading">Лучшие предложения <br>только у нас!</h2>
-							<p class="slide__text-desc"></p> <a href="gallery.html#gallery" class="slide__text-link">перейти</a> 
-						</section>
-					</div>
-				</div>
-			</div>
-			<!-- end slide -->
-			<!-- start slide -->
-			<!-- <div class="slide slide-2">
-				<div class="slide__bg"></div>
-				<div class="slide__content">
-					<div class="container slide__content-wrap">
-						<section class="slide__text">
-							<h2 class="slide__text-heading">Разумные цены и индивидуальный подход</h2>
-							<p class="slide__text-desc"></p> <a href="#" class="slide__text-link">Посмотреть товар</a> 
-						</section>
-					</div>
-				</div>
-			</div> -->
-			<!-- end slide -->
-			<!-- start slide -->
-			<!-- <div class="slide slide-3">
-				<div class="slide__bg"></div>
-				<div class="slide__content">
-					<div class="container slide__content-wrap">
-						<section class="slide__text">
-							<h2 class="slide__text-heading">Профессиональный подход к каждому клиенту</h2>
-							<p class="slide__text-desc"></p> <a href="#" class="slide__text-link">Посмотреть товар</a> 
-						</section>
-					</div>
-				</div>
-			</div> -->
-			<!-- end slide -->
-		</div>
-	</div>
-	<!-- end slider -->
+	<!-- start header-slider -->
+	<?php get_template_part( 'template-parts/header', 'slider' ); ?>
+	<!-- end header-slider -->
 	<!-- main section -->
 	<main class="container-fluid main-section dark" id="description" style="background-image: url(<?php echo get_template_directory_uri() ?>/img/bg-main-section.jpg);">
 		<div class="green-filter"></div>		
---
+++ template-parts/header-slider.php
@@ -0,0 +1,35 @@
+<!-- start slider -->
+<div class="slider-container">
+	<div class="slider-control left inactive"></div>
+	<div class="slider-control right"></div>
+	<ul class="slider-pagi"></ul>
+	<div class="slider">
+		<!-- start slide -->
+		<div class="slide slide-0 active">
+			<div class="slide__bg" style="background-image: url(<?php echo get_template_directory_uri() ?>/img/slide_1.jpg);"></div>
+			<div class="slide__content">
+				<div class="container slide__content-wrap">
+					<section class="slide__text">
+						<h2 class="slide__text-heading">Новые поступления &mdash; <br>новые впечатления!</h2>
+						<p class="slide__text-desc"></p> <a href="catalog.html#brewers" class="slide__text-link">подробнее</a> 
+					</section>
+				</div>
+			</div>
+		</div>
+		<!-- end slide -->
+		<!-- start slide -->
+		<div class="slide slide-1 ">
+			<div class="slide__bg" style="background-image: url(<?php echo get_template_directory_uri() ?>/img/slide_2.jpg);"></div>
+			<div class="slide__content">
+				<div class="container slide__content-wrap">
+					<section class="slide__text">
+						<h2 class="slide__text-heading">Лучшие предложения <br>только у нас!</h2>
+						<p class="slide__text-desc"></p> <a href="gallery.html#gallery" class="slide__text-link">перейти</a> 
+					</section>
+				</div>
+			</div>
+		</div>
+		<!-- end slide -->
+	</div>
+</div>
+	<!-- end slider -->
