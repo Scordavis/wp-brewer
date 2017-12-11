59084273832d1c33e2fcc46479d29d17c76650ec
Vladimir Solomykin <scordavis@gmail.com>
2017-12-11 21:50:05 +0200

    add template content-about
    Добавлен и отредактирован шаблон для блока "О нас". Добавлены ссылки для
    иконок партнеров через произвольные поля.

--- template-parts/content-partners.php
+++ template-parts/content-partners.php
@@ -2,6 +2,5 @@
 		<div class="container partners">
 			<?php if ( have_posts() ) : query_posts('category_name=partners');
     while (have_posts()) : the_post(); ?>  
-<a onclick="window.open('http://www.pivovar-frydlant.com','_blank')"><?php the_post_thumbnail(); ?></a>
 <?php endwhile; endif; wp_reset_query(); ?>
 		</div>
