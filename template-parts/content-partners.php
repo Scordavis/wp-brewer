e3fdaab0b0a2b28640c4fc79c6329368dabfe502
Vladimir Solomykin <scordavis@gmail.com>
2017-12-11 19:23:03 +0200

    add <?php ?> in content-partners.php

--- template-parts/content-partners.php
+++ template-parts/content-partners.php
@@ -1,9 +1,7 @@
 		<!-- start partners -->
 		<div class="container partners">
 			<?php if ( have_posts() ) : query_posts('category_name=partners');
-    while (have_posts()) : the_post(); ?>
-      
+    while (have_posts()) : the_post(); ?>  
 <a onclick="window.open('http://www.pivovar-frydlant.com','_blank')"><?php the_post_thumbnail(); ?></a>
-<? endwhile; endif; wp_reset_query(); ?>
+<?php endwhile; endif; wp_reset_query(); ?>
 		</div>
-		<!-- end partners -->
