927a52826df5727c8a2b364739a95de63daa6512
Vladimir Solomykin <scordavis@gmail.com>
2017-12-11 21:50:43 +0200

    meta fields in PARTNERS

--- template-parts/content-partners.php
+++ template-parts/content-partners.php
@@ -2,5 +2,6 @@
 		<div class="container partners">
 			<?php if ( have_posts() ) : query_posts('category_name=partners');
     while (have_posts()) : the_post(); ?>  
+<a href="<?php echo get_post_meta($post->ID, 'ssylka', true); ?>" target="_blank"><?php the_post_thumbnail(); ?></a>
 <?php endwhile; endif; wp_reset_query(); ?>
 		</div>
